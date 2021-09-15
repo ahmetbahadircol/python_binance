e2e7cf296d65b8b23fd09d5a5a3d93d0e0048287
ahmetbahadircol <ahmetbahadircol@gmail.com>
2021-09-15 23:04:53 +0300

    MACD, signal and fibo calculations are added. Output is reshaped according to willing of Ozcan Cansiz

--- main_calculaiton.py
+++ main_calculaiton.py
@@ -44,14 +44,44 @@ def relative_strength_index(df, n):
     df = df.join(RSI)
     return df
 
+def moving_average_convergence_divergence(df,n1,n2):
+    exp1 = df['close'].ewm(span=n1, adjust=False).mean()
+    exp2 = df['close'].ewm(span=n2, adjust=False).mean()
+    macd = pd.Series((exp1 - exp2), name = 'MACD_' + str(n1) + '_' + str(n2))
+    df = df.join(macd)
+    return df
+    
+
+def MACD_signal(macd,n3):
+    exp3 = pd.Series((macd['close'].ewm(span=n3, adjust=False).mean()), name = 'MACD_signal_' + str(n3))
+    df = macd.join(exp3)
+    return df
+
+def fibo(df,l):
+    for coin in l:
+        selected = df[df['symbol'] == coin]
+        close = selected.tail(1).iloc[0]['close']
+        fibb_list = {}
+        max_c = selected['close'].max()
+        min_c = selected['close'].min()
+        print(coin + '[Close: ' + str(close) + '] - [Fibo: ' 
+             + str((max_c - min_c) * 0) 
+             + ' , ' + str(round(((max_c - min_c) * 0.236),5)) 
+             + ' , ' + str(round(((max_c - min_c) * 0.382),5)) 
+             + ' , ' + str(round(((max_c - min_c) * 0.500),5)) 
+             + ' , ' + str(round(((max_c - min_c) * 0.618),5))
+             + ' , ' + str(round(((max_c - min_c) * 0.786),5))
+             + ' , ' + str(round(((max_c - min_c) * 1.000),5))
+             + ']'
+            )
 def analysis():
     
     symbol_list = get_data.coin_list()
     df = get_data.main(symbol_list)
-    to_buy = []
-    to_sell = []
-    rsi_test = []
-    rsi_test_al  =[]
+    RSI_to_buy = {}
+    RSI_to_sell = {}
+    m_to_buy = []
+    m_to_sell = []
     for coin in symbol_list:
         tmp_df = df[df["symbol"] == coin]
         tmp_df = tmp_df.reset_index()
@@ -62,59 +92,67 @@ def analysis():
         tmp_df = moving_avg(tmp_df, 99)
         tmp_df = exponential_moving_avg(tmp_df, 7)
         tmp_df = exponential_moving_avg(tmp_df, 25)
-        tmp_df = exponential_moving_avg(tmp_df, 99)
+        tmp_df = exponential_moving_avg(tmp_df, 100)
         tmp_df = relative_strength_index(tmp_df, 6)
         tmp_df = relative_strength_index(tmp_df, 12)
-        tmp_df = relative_strength_index(tmp_df, 24)
+        #tmp_df = relative_strength_index(tmp_df, 24)
+        tmp_df = moving_average_convergence_divergence(tmp_df,12,26)
+        tmp_df = MACD_signal(tmp_df,9)
 
-        tmp_df = tmp_df.tail(1)
-        print(tmp_df)
+        tmp_df_last = tmp_df.tail(1)
+        #print(tmp_df_last)
 
-        close = tmp_df.iloc[0]["close"]
+        close = tmp_df_last.iloc[0]["close"]
 
-        MA_7 = tmp_df.iloc[0]["MA_7"]
-        MA_25 = tmp_df.iloc[0]["MA_25"]
-        MA_99 = tmp_df.iloc[0]["MA_99"]
-        EMA_7 = tmp_df.iloc[0]["EMA_7"]
-        EMA_25 = tmp_df.iloc[0]["EMA_25"]
-        EMA_99 = tmp_df.iloc[0]["EMA_99"]
-        RSI_6 = tmp_df.iloc[0]["RSI_6"]
-        RSI_12 = tmp_df.iloc[0]["RSI_12"]
-        RSI_24 = tmp_df.iloc[0]["RSI_24"]
+        MA_7 = tmp_df_last.iloc[0]["MA_7"]
+        MA_25 = tmp_df_last.iloc[0]["MA_25"]
+        MA_99 = tmp_df_last.iloc[0]["MA_99"]
+        EMA_7 = tmp_df_last.iloc[0]["EMA_7"]
+        EMA_25 = tmp_df_last.iloc[0]["EMA_25"]
+        EMA_100 = tmp_df_last.iloc[0]["EMA_100"]
+        RSI_6 = tmp_df_last.iloc[0]["RSI_6"]
+        RSI_12 = tmp_df_last.iloc[0]["RSI_12"]#14
+        #RSI_24 = tmp_df_last.iloc[0]["RSI_24"]
+        MACD_12_26 = tmp_df_last.iloc[0]["MACD_12_26"]
+        MACDsignal = tmp_df_last.iloc[0]["MACD_signal_9"]
 
-        if ((close <= MA_7) & (close >= MA_25) & (close >= MA_99) & (close <= EMA_7) & (close >= EMA_25) & (close >= EMA_99) & ((RSI_6 <= 0.2) | (RSI_12 <= 0.2))):
-            to_buy.append(coin)
-        if ((close >= MA_7) & (close <= MA_25) & (close <= MA_99) & (close >= EMA_7) & (close <= EMA_25) & (close <= EMA_99) & ((RSI_6 >= 0.8) | (RSI_12 >= 0.8))):
-            to_sell.append(coin)
+        if ((close <= MA_7) & (close >= MA_25) & (close >= MA_99) & (close <= EMA_7) & (close >= EMA_25) & (close >= EMA_100) & ((RSI_6 <= 0.2) | (RSI_12 <= 0.2))):
+            pass
+        if ((close >= MA_7) & (close <= MA_25) & (close <= MA_99) & (close >= EMA_7) & (close <= EMA_25) & (close <= EMA_100) & ((RSI_6 >= 0.8) | (RSI_12 >= 0.8))):
+            pass
+
         if (RSI_6 >= 0.65) | (RSI_12 >= 0.65):
-            rsi_test.append(coin)
+            RSIlist = []
+            RSIlist.append(RSI_6)
+            RSIlist.append(RSI_12)
+            RSI_to_buy[coin] = RSIlist
+
         if (RSI_6 <= 0.35) | (RSI_12 <= 0.35):
-            rsi_test_al.append(coin)
+            RSIlist = []
+            RSIlist.append(RSI_6)
+            RSIlist.append(RSI_12)
+            RSI_to_sell[coin] = RSIlist
+        
+        if (close > EMA_100) & (MACD_12_26 > MACDsignal):
+            m_to_buy.append(coin)
+        
+        if (close < EMA_100) & (MACD_12_26 < MACDsignal):
+            m_to_sell.append(coin)
 
+    print('MACDye(' + tmp_df.columns[15] + ') göre ALINACAKLAR ve Fibonacci değerleri:\n')
+    fibo(df,m_to_buy)
+    print('-' * 100)
+    print('MACDye(' + tmp_df.columns[15] + ') göre SATILACAKLAR ve Fibonacci değerleri:\n')
+    fibo(df,m_to_sell)
+    print('-' * 100)
+    print('RSIya göre ALINACAKLAR ve RSI değerleri(' + tmp_df.columns[13] + ' ve ' + tmp_df.columns[14] + '):\n')
+    for coin in RSI_to_buy:
+        print(coin , [round(x,5) for x in RSI_to_buy[coin]])
+    print('-' * 100) 
+    print('RSIya göre SATILACAKLAR ve RSI değerleri(' + tmp_df.columns[13] + ' ve ' + tmp_df.columns[14] + '):\n')
+    for coin in RSI_to_sell:
+        print(coin , [round(x,5) for x in RSI_to_sell[coin]])       
 
-    print('Alınacaklar: ')
-    print(to_buy)
-    print('Satılacaklar: ')
-    print(to_sell)
-    print('Test List - sell')
-    print(rsi_test)
-    print('Test List - buy')
-    print(rsi_test_al)
 
 analysis() 
 
-'''
-Alış için:
- 1)   eğer close <= MA_7 ve close >= MA_25 ve close >= MA_99 
-
- 2)   eğer close <= EMA_7 ve close >= EMA_25 ve close >= EMA_99 
-
- 3)   eğer RSI_6 <= 20 veya RSI_12 <= 20
-
- Satış için:
- 1)   eğer close >= MA_7 ve close <= MA_25 ve close <= MA_99 
-
- 2)   eğer close >= EMA_7 ve close <= EMA_25 ve close <= EMA_99 
-
- 3)   eğer RSI_6 >= 80 veya RSI_12 >= 80
-'''
